450e0126ed5469d6fd64c231182dd1cf028e3499
Srohith3009 <159873530+Srohith3009@users.noreply.github.com>
2024-02-28 10:39:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,140 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+2024-02-28 05:08:26.844 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/first_streamlit_app/streamlit_app.py", line 40, in <module>
+
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/__init__.py", line 54, in Connect
+
+    return SnowflakeConnection(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 429, in __init__
+
+    self.connect(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 683, in connect
+
+    self.__config(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 1180, in __config
+
+    Error.errorhandler_wrapper(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 290, in errorhandler_wrapper
+
+    handed_over = Error.hand_to_other_handler(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 348, in hand_to_other_handler
+
+    connection.errorhandler(connection, cursor, error_class, error_value)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 221, in default_errorhandler
+
+    raise error_class(
+
+snowflake.connector.errors.ProgrammingError: 251006: Password is empty
+
+2024-02-28 05:08:26.989 503 GET /script-health-check (10.13.59.71) 973.01ms
+
+2024-02-28 05:08:31.704 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/first_streamlit_app/streamlit_app.py", line 40, in <module>
+
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/__init__.py", line 54, in Connect
+
+    return SnowflakeConnection(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 429, in __init__
+
+    self.connect(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 683, in connect
+
+    self.__config(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 1180, in __config
+
+    Error.errorhandler_wrapper(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 290, in errorhandler_wrapper
+
+    handed_over = Error.hand_to_other_handler(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 348, in hand_to_other_handler
+
+    connection.errorhandler(connection, cursor, error_class, error_value)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 221, in default_errorhandler
+
+    raise error_class(
+
+snowflake.connector.errors.ProgrammingError: 251006: Password is empty
+
+2024-02-28 05:08:31.836 503 GET /script-health-check (10.13.59.71) 904.35ms
+
+2024-02-28 05:08:36.684 Uncaught app exception
+
+Traceback (most recent call last):
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+
+    exec(code, module.__dict__)
+
+  File "/mount/src/first_streamlit_app/streamlit_app.py", line 40, in <module>
+
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/__init__.py", line 54, in Connect
+
+    return SnowflakeConnection(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 429, in __init__
+
+    self.connect(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 683, in connect
+
+    self.__config(**kwargs)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/connection.py", line 1180, in __config
+
+    Error.errorhandler_wrapper(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 290, in errorhandler_wrapper
+
+    handed_over = Error.hand_to_other_handler(
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 348, in hand_to_other_handler
+
+    connection.errorhandler(connection, cursor, error_class, error_value)
+
+  File "/home/adminuser/venv/lib/python3.9/site-packages/snowflake/connector/errors.py", line 221, in default_errorhandler
+
+    raise error_class(
+
+snowflake.connector.errors.ProgrammingError: 251006: Password is empty
+
+2024-02-28 05:08:36.835 503 GET /script-health-check (10.13.59.71) 906.38ms
